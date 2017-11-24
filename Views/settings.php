5a91ae6a02ca0c28e304c4baa9f2d0011fcc8fbb
Caroline Bieuvelet <caroline.bieuvelet@gmail.com>
2017-11-24 10:46:31 +0100

    modif header + settings + css caro

--- Views/settings.php
+++ Views/settings.php
@@ -3,21 +3,30 @@
 <div class="mainSettings">
   <form action="" method="post">
     <section class="personalInfo">
-      <label for="name">Name : </label>
-      <input type="text" id="name"/><br/>
-      <label for="email">Email : </label>
-      <input type="text" id="email"/><br/>
-      <label for="newPass">New password : </label>
-      <input type="text" id="newPass"/><br/>
-      <label for="newPassCheck">New password (check) : </label>
-      <input type="text" id="newPassCheck"/><br/>
-      <label for="newProfilePic">New profile's picture : </label>
-      <input type="file" id="newProfilePic"/><br/>
-      <img src="" alt="currentProfilePic"/>
+      <div>
+        <label for="name">Name : </label>
+        <input type="text" id="name"/>
+      </div>
+      <div>
+        <label for="email">Email : </label>
+        <input type="text" id="email"/>
+      </div>
+      <div>
+        <label for="newPass">New password : </label>
+        <input type="text" id="newPass"/>
+      </div>
+      <div>
+        <label for="newPassCheck">New password (check) : </label>
+        <input type="text" id="newPassCheck"/>
+      </div>
+      <div>
+        <label for="newProfilePic">New profile's picture : </label>
+        <input type="file" id="newProfilePic"/>
+      </div>
     </section>
     <section class="buttonsSettingsPage">
       <input type="button" name="cancel" value="Cancel"/>
-      <input type="submit" name="submit" value="Save"/>
+      <input type="submit" name="submit" value="Save" class="save"/>
     </section>
   </form>
 </div>
