75ce6c7f97fa02c57019d84fd797e0f0fa6eb28a
OrnellaSimba <ornellasimba@gmail.com>
2017-11-26 19:33:48 +0100

    update

--- upload.php
+++ upload.php
@@ -3,13 +3,15 @@
 /**
  * Partie qui va gérer la reception des image après upload
  */
+ 
 
 if(!empty($_FILES)){
 $img = $_FILES['img'];
 $extensions= strtolower(substr($img['name'], -3));
 $extensions_valides = array( 'jpg' , 'gif' , 'png' );
 if (in_array($extensions,$extensions_valides)) {
-  move_uploaded_file($img['tmp_name'], "images/". $img['name']);
+  $newId= md5(uniqid(rand(), true));
+  move_uploaded_file($img['tmp_name'], "images/". $img['name'].$newId);
   echo "transfert reussi !";
 }else {
   $erreur = "Erreur, fichier non autorisé !";
--- uploadForm.php
+++ uploadForm.php
@@ -2,12 +2,5 @@
      <input type="file" name="img" value="img">
      <input type="submit" name="submit" value="envoyer">
 
-     <!-- <label for="mon_fichier">Fichier ( max. 1 Mo) :</label><br />
-     <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
-     <input type="file" name="mon_fichier" id="mon_fichier" /><br />
-     <label for="titre">Titre du fichier (max. 50 caractères) :</label><br />
-     <input type="text" name="titre" value="Titre du fichier" id="titre" /><br />
-     <label for="description">Description de votre fichier (max. 255 caractères) :</label><br />
-     <textarea name="description" id="description"></textarea><br />
-     <input type="submit" name="submit" value="Envoyer" /> -->
+     <!-- Formulaire temporaire chercher le bon -->
 </form>
