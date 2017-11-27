1267bd5c62ca4b9bb67e2347f98840d9243e49e2
jimmyGoossens <jimmy.v.goossens@gmail.com>
2017-11-27 11:31:13 +0100

    update class pdo

--- class_PDO.php
+++ class_PDO.php
@@ -43,7 +43,7 @@ class pdoModel{
 		return $req;
 	}
 
-}
+
 		function delete() {
 
 		$requete = $bdd->prepare('DELETE FROM image WHERE name=:img ');
@@ -53,6 +53,23 @@ class pdoModel{
 
 
 	}
+	 	function upload() {
+
+			if(!empty($_FILES)){
+				$img = $_FILES['img'];
+				$extensions= strtolower(substr($img['name'], -3));
+				$extensions_valides = array( 'jpg' , 'gif' , 'png' );
+				if (in_array($extensions,$extensions_valides)) {
+				  $newId= md5(uniqid(rand(), true));
+				  move_uploaded_file($img['tmp_name'], "images/". $img['name'].$newId);
+				  echo "transfert reussi !";
+				}else {
+				  $erreur = "Erreur, fichier non autorisé !";
+				  echo "$erreur";
+				}
+			}
+	  }
+}
 /*
 	function update(){
 
