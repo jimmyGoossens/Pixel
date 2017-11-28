ebdfbe36e91f8cf11d066f37924fee0e5fcac911
Mutwakil MO <mutwakil.m.osman@gmail.com>
2017-11-28 16:48:52 +0100

    last

---
+++ Assets/js/home.js
@@ -0,0 +1,9 @@
+var $grid = $('.grid').masonry({
+itemSelector: '.grid-item',
+percentPosition: true,
+columnWidth: '.grid-sizer'
+});
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+$grid.masonry();
+});
